2a7aefce68daac99ce2e3ea344a25f32fc640320
Luke Olson <luke.olson@gmail.com>
2021-01-22 10:27:23 -0600

    update date to 29-2

--- make.py
+++ make.py
@@ -17,17 +17,19 @@ if os.path.exists(liveweb):
 
 os.makedirs(liveweb)
 
+
 # remove blank entries from the yaml input
 def prune_blank(somelist, key):
     somelist = [c for c in somelist if c[key] is not None]
     return somelist
+
 
 # parse, render each template here
 env = Environment(loader=FileSystemLoader('./'))
 files = ['_index.html']
 
 with io.open("./data/conferences.yml", "r") as inf:
-    conferences = yaml.load(inf)
+    conferences = yaml.safe_load(inf)
     conferences = prune_blank(conferences, 'year')
     # now order by years
     conferences = sorted(conferences,
